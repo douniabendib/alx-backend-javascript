ec455ca52ea3746ee6ffe4f377ab0b00cf3c0e67
bendib.dounia <bendib.dounia@gmail.com>
2024-03-06 23:55:54 +0100

    done

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,16 +1,4 @@
-// Return a promise from a function
+/* eslint-disable no-unused-vars */
 export default function getResponseFromAPI() {
-    // Return a new promise
-    return new Promise((resolve, reject) => {
-      // Do some asynchronous work
-      setTimeout(() => {
-        try {
-          // Fulfill the promise with a value
-          resolve('Response from API');
-        } catch (error) {
-          // Reject the promise with a reason
-          reject(error);
-        }
-      }, 1000);
-    });
-  }
+  return new Promise((resolve, reject) => {});
+}
