b38a09fcbe9882c624f30af293d76612fcdb96c7
douniabendib <bendib.dounia@gmail.com>
2024-03-06 23:00:00 +0100

    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,16 @@
-function getResponseFromAPI() {
+// Return a promise from a function
+export default function getResponseFromAPI() {
+    // Return a new promise
     return new Promise((resolve, reject) => {
-      // Simulating an asynchronous API call
+      // Do some asynchronous work
       setTimeout(() => {
-        const data = { message: "API response" };
-        resolve(data);
-      }, 2000); 
+        try {
+          // Fulfill the promise with a value
+          resolve('Response from API');
+        } catch (error) {
+          // Reject the promise with a reason
+          reject(error);
+        }
+      }, 1000);
     });
   }
-  
-  export default getResponseFromAPI;
