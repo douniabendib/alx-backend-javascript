8f2ec6287b4c660008ad5bc9ff4e2d062d054db6
douniabendib <bendib.dounia@gmail.com>
2024-03-12 15:21:27 +0000

    create array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+const getListStudents = () => [
+  {id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+  {id: 2, firstName: 'James', location: 'Columbia' },
+  {id: 5, firtName: 'Serena', location: 'San Francisco' },
+];
+
+export default getListStudents;
